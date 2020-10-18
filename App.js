e3cfabf695c286e2b9c95fc1c2dd0a63e37ccefd
enuii3 <b.nxxxi551@gmail.com>
2020-10-19 00:42:17 +0900

    ファンクションコンポーネントからクラスコンポーネントへの書き換え

--- App.js
+++ App.js
@@ -7,37 +7,61 @@ import axios from 'axios'
 
 const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
 
-export default function App() {
-  const [articles, setArticles] = useState([])
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: [],
+    }
+  }
 
-  useEffect(() => {
-    fetchArticles()
-  }, [])
+  // 上記は下記のクラスコンポーネント版
+  // const [articles, setArticles] = useState([])
+  // useEffect(() => {
+  //   fetchArticles()
+  // }, [])
 
-  async function fetchArticles(){
+  async componentDidMount(){
+    this.fetchArticles(URL)
+  }
+
+  fetchArticles = async () => {
     try{
       const response = await axios.get(URL)
-      setArticles(response.data.articles)
+      this.setState({ articles: response.data.articles })
     }catch(error){
       console.error(error)
     }
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(article, index) => index.toString()}
-      />
-    </SafeAreaView>
-  )
+  // 上記は下記のクラスコンポーネント版
+  // const [articles, setArticles] = useState([])
+  // async function fetchArticles(){
+  //   try{
+  //     const response = await axios.get(URL)
+  //     setArticles(response.data.articles)
+  //   }catch(error){
+  //     console.error(error)
+  //   }
+  // }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={this.state.articles}
+          renderItem={({ item }) => (
+            <ListItem
+              imageUrl={item.urlToImage}
+              title={item.title}
+              author={item.author}
+            />
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
